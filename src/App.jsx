76004c4fd8ad363645cad6f7caf73c95205caa71
ousafa <ouaamou.safa@gmail.com>
2025-09-12 15:55:00 +0100

    TODO-04-Custom font using (Font Face)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Lists from './components/Lists.jsx';
 import './App.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "Alexandria",
+    }
+})
 
 function App() {
 
   return (
-    <div className="App">
-        <Lists />
-    </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+            <Lists />
+        </div>
+      </ThemeProvider>
   )
 }
 
--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -42,6 +42,7 @@ const Lists = () => {
                 </Card>
             </Container>
         </>
+
     );
 };
 
