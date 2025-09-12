3ec67fe6a9d705b62e0573b73e4370bfcde02fe8
ousafa <ouaamou.safa@gmail.com>
2025-09-12 12:46:01 +0100

    TODO-00-Upload Libraries & Setup the Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import List from './components/List';
+import './App.css'
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+function App() {
+
+  return (
+    <>
+        <List />
+        <AddCircleIcon/>
+    </>
+  )
+}
+
+export default App
