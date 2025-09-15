c42360e27440546bbb18c3c3e50ebbc1a33e7078
ousafa <ouaamou.safa@gmail.com>
2025-09-15 12:17:41 +0100

    TODO-09-Deleteing Todo

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,16 @@
-import React, {useContext} from 'react';
-import {Card, CardContent, Grid, Typography} from "@mui/material";
+import React, {useContext, useState} from 'react';
+import {
+    Button,
+    Card,
+    CardContent,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Grid,
+    Typography
+} from "@mui/material";
 
 // ICONS
 import CheckIcon from "@mui/icons-material/Check";
@@ -12,8 +23,10 @@ import {TodosContext} from "../contexts/todosContext.js";
 
 const List = ({todo: {id,title,details,isCompleted}}) => {
 
+    const [open, setOpen] = useState(false);
     const {todos,setTodos} = useContext(TodosContext);
 
+    // EVENT HANDLERS
     function handleCheckClick() {
         const updatedTodos = todos.map((todo) =>
             todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
@@ -21,8 +34,49 @@ const List = ({todo: {id,title,details,isCompleted}}) => {
         setTodos(updatedTodos);
     }
 
+    function handleDeleteClick() {
+        setOpen(true);
+    }
+
+    function handleClose()  {
+        setOpen(false);
+    }
+
+    function handleDeleteConfirm() {
+        const updatedTodos = todos.filter((todo)=>{
+            return todo.id !== id;
+        })
+        setTodos(updatedTodos);
+    }
+
+
+    // === EVENT HANDLERS ===
+
     return (
         <>
+           {/* DELETE DIALOG */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    هل أنت متأكد من رغبتك في حذف المهمة؟
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        لا يمكنك التراجع عن الحذف بعد إتمامه.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} style={{color:'#96173D'}}>إغلاق</Button>
+                    <Button onClick={handleDeleteConfirm} autoFocus style={{color:'#96173D'}}>
+                        نعم، قم بالحذف
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            {/* === DELETE DIALOG === */}
             <Card className="list-card" sx={{ minWidth: 275, background:"#4C4BD2", color:'white',marginTop:5 }}>
                 <CardContent>
                     <Grid container spacing={2}>
@@ -62,6 +116,7 @@ const List = ({todo: {id,title,details,isCompleted}}) => {
                             >
                                 <ModeEditOutlineOutlinedIcon />
                             </IconButton>
+                            {/* DELETE ICON BUTTON */}
                             <IconButton
                                 className="iconButton"
                                 aria-label="delete"
@@ -70,9 +125,11 @@ const List = ({todo: {id,title,details,isCompleted}}) => {
                                     background: "white",
                                     border: "solid #b23c17 3px",
                                 }}
+                                onClick={handleDeleteClick}
                             >
                                 <DeleteOutlineOutlinedIcon />
                             </IconButton>
+                            {/* === DELETE ICON BUTTON === */}
                         </Grid>
                         {/* === ACTION BUTTONS === */}
                     </Grid>
